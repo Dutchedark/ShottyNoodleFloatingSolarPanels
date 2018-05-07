0f969e40d3ed1315b737a6433f689da2f010681e
Dutchedark <33281828+Dutchedark@users.noreply.github.com>
2018-05-07 16:55:36 +0200

    Daily Push
    A Push A Day Keeps Mehni's Wraith Away

--- Source/ShottyNoodleFloatingSolarPanels.cs
+++ Source/ShottyNoodleFloatingSolarPanels.cs
@@ -1,4 +1,10 @@
-﻿namespace ShottyNoodleFloatingSolarPanels
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace ShottyNoodleFloatingSolarPanels
 {
     public class ShottyNoodleFloatingSolarPanels
     {
