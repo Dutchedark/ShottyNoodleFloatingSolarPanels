40b1246545831145519445d3f2cbf85214a9797f
Dutchedark <33281828+Dutchedark@users.noreply.github.com>
2018-05-08 11:25:38 +0200

    Daily push
    A Push A Day Keeps Mehni's Wraith Away
    
    also added the conduits

--- Source/ShottyNoodleFloatingSolarPanels.cs
+++ Source/ShottyNoodleFloatingSolarPanels.cs
@@ -6,7 +6,24 @@ using Verse;
 
 namespace ShottyNoodleFloatingSolarPanels
 {
-    public class ShottyNoodleFloatingSolarPanels
-    {
+    public class PlaceWorker_FloatableSolarPanel : PlaceWorker {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null){
+            TerrainDef terraindef = map.terrainGrid.TerrainAt(loc);
+            if (terraindef == TerrainDef.Named("WaterDeep") || terraindef == TerrainDef.Named("WaterOceanDeep") || terraindef == TerrainDef.Named("WaterMovingDeep")) {
+               return true;
+            }
+            return new AcceptanceReport("SS.OnlyDeepWater".Translate());
+        }
+    }
+
+    public class PlaceWorker_FloatableConduit : PlaceWorker {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null){
+            TerrainDef terraindef = map.terrainGrid.TerrainAt(loc);
+            if (terraindef == TerrainDef.Named("WaterDeep") || terraindef == TerrainDef.Named("WaterOceanDeep") || terraindef == TerrainDef.Named("WaterMovingDeep") ||
+                 terraindef == TerrainDef.Named("WaterShallow") || terraindef == TerrainDef.Named("WaterOceanShallow") || terraindef == TerrainDef.Named("WaterMovingShallow")) {
+               return true;
+            }
+            return new AcceptanceReport("SS.OnlyWater".Translate());
+        }
     }
 }
