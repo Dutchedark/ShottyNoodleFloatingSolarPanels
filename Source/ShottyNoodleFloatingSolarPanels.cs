e59a6baeec70b5eb783c937005c65d480707a0ba
Dutchedark <33281828+Dutchedark@users.noreply.github.com>
2018-05-14 10:32:12 +0200

    Building over deep water patch
    some more wizardry for building over deepwater

--- Source/ShottyNoodleFloatingSolarPanels.cs
+++ Source/ShottyNoodleFloatingSolarPanels.cs
@@ -26,4 +26,37 @@ namespace ShottyNoodleFloatingSolarPanels
             return new AcceptanceReport("SS.OnlyWater".Translate());
         }
     }
+
+    public class Building_FloatableSolarPanel : Building
+    {
+        public string TerrainTypeAtBaseCellDefAsString;
+        public override void Destroy(DestroyMode mode = 0)
+        {
+            base.Map.snowGrid.SetDepth(base.Position, 0f);
+            if (this.TerrainTypeAtBaseCellDefAsString != null)
+            {
+                base.Map.terrainGrid.SetTerrain(base.Position, TerrainDef.Named(this.TerrainTypeAtBaseCellDefAsString));
+            }
+            base.Destroy(mode);
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<string>(ref this.TerrainTypeAtBaseCellDefAsString, "TerrainTypeAtBaseCellDefAsString", null, false);
+        }
+
+        public override void SpawnSetup(Map map, bool flag)
+        {
+            base.SpawnSetup(map, flag);
+            TerrainDef terrainDef = map.terrainGrid.TerrainAt(base.Position);
+
+            if (terrainDef.defName.Contains("Water"))
+            {
+                if (terrainDef == TerrainDef.Named("WaterDeep") || terrainDef == TerrainDef.Named("WaterOceanDeep") || terrainDef == TerrainDef.Named("WaterMovingDeep"))
+                {
+                    map.terrainGrid.SetTerrain(base.Position, TerrainDef.Named("UnderPanelWater"));
+                }
+            }
+        }
+    }
 }
